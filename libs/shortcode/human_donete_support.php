3cb41340731452e89fa29d1450c37fe1f5e2668d
pro-mannan <promasudbd@gmail.com>
2023-02-02 23:26:25 +0600

    add now shortcode today

--- libs/shortcode/human_donete_support.php
+++ libs/shortcode/human_donete_support.php
@@ -2,63 +2,35 @@
 /* human appeal first shorcode create heere now*/
 
 /* */
-add_shortcode( 'human_donete_support', 'human_donete_support' );
+add_shortcode( 'human_donete_support', 'human_donete_supports' );
 
-function human_donete_support( $atts ) {
-         extract( 
-           shortcode_atts([
-              'project_title'                       => '',
-              'project_img'                         => '',
-              'project_bg'                          => '',
-              'project_model_title'                 => '',
-              'project_model_desc'                  => '',
-              'project_model_button_text_one'       => '',
-              'project_model_button_url_one'        => '',
-              'project_model_button_text_two'       => '',
-              'project_model_button_url_two'        => '',
-              'project_model_you_link'              => '',
-              'project_model_img'                   => '',
+function human_donete_supports( $atts ) {
+         ob_start();
+              $atts = shortcode_atts(array(
+                  'box_repeater_items' =>'',        
+              ), $atts, 'human_donete_support');
 
-         ],$atts ) );
+           
+              $items = vc_param_group_parse_atts($atts['box_repeater_items']);
 
-
+      ?>
   
 
- return "<div class='watch' style='background-image: url(\"./images/others/wbg.png\")'>
-            <div class='container'>
+ ?>
+            <div class='TopwatchFe'>
                 <div class='row'>
-                    <div class='col'>
-                        <div class='watching>
-                            <h3>watch</h3>
-                            <a href=''><img src='./images/others/w.png' alt='watch'></a>
-                        </div>
-                        <div class='TopwatchFe'>
-                            <div class='row'>
-                                <div class='col-sm-4'>
-                                    <div class='TopwatchFe_item'>
-                                        <h3>DONATE</h3>
-                                        <p>Support our work</p>
-                                    </div>
-                                </div>
-                                <div class='col-sm-4'>
-                                    <div class='TopwatchFe_item'>
-                                        <h3>DONATE</h3>
-                                        <p>Support our work</p>
-                                    </div>
-                                </div>
-                                <div class='col-sm-4'>
-                                    <div class='TopwatchFe_item'>
-                                        <h3>DONATE</h3>
-                                        <p>Support our work</p>
-                                    </div>
-                                </div>
+                    <?php foreach( $items as $item ) { ?>
+                        <div class='col-sm-4'>
+                            <div class='TopwatchFe_item'>
+                                <h3><?php echo $item['donete_title']; ?></h3>
+                                <p>Support our work</p>
                             </div>
                         </div>
-                    </div>
+                    <?php } ?>
                 </div>
             </div>
-        </div>
- 		";
+
+ 		<?php 
 }
 ?>
 
@@ -67,78 +39,35 @@ add_action( 'vc_before_init', 'human_donete_support_vc' );
 
 function human_donete_support_vc() {
  vc_map([
-  "name" => __( "Donete Supports Box", "human-appeal" ),
+  "name" => __( "Donete Supports Boxs", "human-appeal" ),
   "base" => "human_donete_support",
   "icon"    => get_template_directory_uri(). '/access/images/logo/logo.png',
   "category" => __( "Human Appleal", "human-appeal"),
-  "params" =>[
-            [
-              "param_name" => "project_title",
-              "type"      => "textfield",
-              "heading"   => __( "Project Title", "human-appeal" ),
-              "description" => __( "Project Item", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_img",
-              "type"       => "attach_image",
-              "heading"    => __( "Upload Project Image", "human-appeal" ),
-              "description" => __( "Project Images", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_bg",
-              "type"      => "colorpicker",
-              "heading"   => __( "Project Background", "human-appeal" ),
-              "description" => __( "Project Item Color", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_title",
-              "type"      => "textfield",
-              "heading"   => __( "Model Title", "human-appeal" ),
-              "description" => __( "Popup Model Title", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_desc",
-              "type"       => "textarea",
-              "heading"    => __( "Model Description", "human-appeal" ),
-              "description" => __( "Popup Model Description", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_button_text_one",
-              "type"       => "textfield",
-              "heading"    => __( "Button One Text", "human-appeal" ),
-              "description" => __( "Botton 1 URL", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_button_url_one",
-              "type"       => "textfield",
-              "heading"    => __( "Botton URL", "human-appeal" ),
-              "description" => __( "Botton 1 URL", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_button_text_two",
-              "type"       => "textfield",
-              "heading"    => __( "Button Two Text", "human-appeal" ),
-              "description" => __( "Botton 2 URL", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_button_url_two",
-              "type"       => "textfield",
-              "heading"    => __( "Botton URL", "human-appeal" ),
-              "description" => __( "Botton 2 URL", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_you_link",
-              "type"       => "textfield",
-              "heading"    => __( "YouTube Video URL", "human-appeal" ),
-              "description" => __( "Put Your YouTube Link", "human-appeal" )
-            ],
-            [
-              "param_name" => "project_model_img",
-              "type"       => "attach_image",
-              "heading"    => __( "Model Popup Image", "human-appeal" ),
-              "description" => __( "Upload Your Model Images", "human-appeal" )
-            ]
-        ]
+  "params" => [
+                [
+                'type' => 'param_group',
+                'param_name' => 'box_repeater_items',
+                "icon"    => get_template_directory_uri(). '/access/images/logo/logo.png',
+                "category" => __( "Human Appleal", "human-appeal"),
+                'params' => [
+                                 
+                             [
+                                  "param_name" => "donete_title",
+                                  "type" => "textfield",
+                                  "heading" => __("Title", "human-appeal"),
+                                  "value" => __("", "human-appeal"),
+                              ],
+                              [
+                                  "param_name" => "descrip",
+                                  "type" => "textarea",                            
+                                  "heading" => __("Description", "human-appeal"),
+                                  "value" => __("", "human-appeal"),
+                              ]
+                        ]
+                ],                    
+          ]
+
+
     ]);
 }
 ?>
